f95a4d28d5419744df14447f4ea9a07aeacd7c2b
GlitchYou <38540168+GlitchYou@users.noreply.github.com>
2021-12-01 15:34:10 -0300

    Add files via upload

--- fish/launch-completion.fish
+++ fish/launch-completion.fish
@@ -4,5 +4,6 @@ complete -c launch -e # --erase
 complete -c launch -f # --no-files
 complete -c launch -a (apps) -r -s a -l app -d 'open app'
 complete -c launch -a (apps) -r -s i -l info -d 'open app settings' 
+complete -c launch -r -s p -l package -d 'open app from package'
 complete -c launch -s u -l update -d 'load all apps'
 complete -c launch -s h -l help -d 'show help'
